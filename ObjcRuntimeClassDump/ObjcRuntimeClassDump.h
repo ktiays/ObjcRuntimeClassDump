4d634f04c4fc2980ee1aa11e8de21692385c25f8
ktiays <cuansky@gmail.com>
2024-09-01 01:27:10 +0800

    Initial Commit

---
+++ ObjcRuntimeClassDump/ObjcRuntimeClassDump.h
@@ -0,0 +1,16 @@
+//
+//  Created by ktiays on 2024/9/1.
+//  Copyright (c) 2024 ktiays. All rights reserved.
+// 
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjcRuntimeClassDump.
+FOUNDATION_EXPORT double ObjcRuntimeClassDumpVersionNumber;
+
+//! Project version string for ObjcRuntimeClassDump.
+FOUNDATION_EXPORT const unsigned char ObjcRuntimeClassDumpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcRuntimeClassDump/PublicHeader.h>
+
+
