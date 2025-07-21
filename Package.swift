9b8210d27933412c5324936c674e7069f74ef1c0
ktiays <cuansky@gmail.com>
2025-07-21 23:25:53 +0800

    Update macOS platform version to 14 in Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     name: "ObjcRuntimeClassDump",
     platforms: [
         .iOS(.v17),
-        .macOS(.v10_15),
+        .macOS(.v14),
     ],
     products: [
         .library(
